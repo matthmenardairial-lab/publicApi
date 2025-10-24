15706c6f2a423be501821e94a37e08a66bb8e6ca
Mathieu Ménard <mathieu.menard@realdolmen.com>
2025-10-24 12:45:07 +0200

    Add logs again

--- src/main/java/be/ibgebim/testapi3/HalloController.java
+++ src/main/java/be/ibgebim/testapi3/HalloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HalloController {
     @GetMapping("/hello")
     public String hello() {
+        System.out.println("handleEvent called");
         return "Hello Wordddddld!";
     }
 }
